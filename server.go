1c5811c26c7c3943ad1df4c7af3c771f3d44396b
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2023-08-16 16:50:18 +0800

    test

--- server.go
+++ server.go
@@ -8,7 +8,7 @@ import (
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Received request from %s\n", r.RemoteAddr)  // Logs the IP address of the client making the request.
-	fmt.Fprintf(w, "<h1>Hello World</h1> <p> Let's fucking go<span> yessirr</span></p>")
+	fmt.Fprintf(w, "<h1>Hello World</h1> <p> Let's fucking go<span> yessirr</span></p> <li>https</li>")
 }
 
 func main() {
